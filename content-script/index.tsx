f8c147d8a69e1dd92eff9e6eace62359d9cdba09
karankiri <karan.kiri@fabric.inc>
2023-07-31 11:12:51 +0530

    inital setup

--- content-script/index.tsx
+++ content-script/index.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-const pluginTagId = 'extension-root';
-const existingInstance = document.getElementById('extension-root');
+const pluginTagId = "extension-root";
+const existingInstance = document.getElementById("extension-root");
 if (existingInstance) {
-  console.log('existing instance found, removing');
+  console.log("existing instance found, removing");
   existingInstance.remove();
 }
 
-const index = document.createElement('div')
+const index = document.createElement("div");
 index.id = pluginTagId;
 
 // Make sure the element that you want to mount the app to has loaded. You can
@@ -19,13 +19,14 @@ index.id = pluginTagId;
 //
 // Also control when the content script is injected from the manifest.json:
 // https://developer.chrome.com/docs/extensions/mv3/content_scripts/#run_time
-const body = document.querySelector('body')
+const body = document.querySelector("body");
+console.log("abcbc, from content script");
 if (body) {
-  body.append(index)
+  body.append(index);
 }
 
 ReactDOM.createRoot(index).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
