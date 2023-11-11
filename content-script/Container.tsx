986184a296f207f6b95e2545b9b8730e1142e553
Karan Kiri <karankiri.96@gmail.com>
2023-11-11 14:52:41 +0530

    fixes

--- content-script/Container.tsx
+++ content-script/Container.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Container = ({ children }) => {
   return (
-    <div className="absolute top-0 right-0 overflow-y-scroll" style={{ zIndex: 500000 }}>
-      <div className="flex flex-col p-4 shadow-sm rounded-md" style={{ width: '450px' }}>
+    <div className="absolute top-0 right-0 overflow-y-auto shadow-sm rounded-md p-5 fixed bg-background text-foreground" style={{ zIndex: 500000 }}>
+      <div className="flex flex-col p-4" style={{ width: '450px' }}>
         {children}
       </div>
     </div>
