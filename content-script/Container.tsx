6f940c7cef23e5ae7d4f7d67d6fdb66b61e8401a
Karan Kiri <karankiri.96@gmail.com>
2023-11-12 11:39:36 +0530

    changes

--- content-script/Container.tsx
+++ content-script/Container.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Container = ({ children }) => {
   return (
     <div className="top-0 right-0 overflow-y-auto shadow-sm rounded-md p-5 fixed bg-background text-foreground" style={{ zIndex: 500000 }}>
-      <div className="flex flex-col p-4" style={{ width: '450px' }}>
+      <div className="flex flex-col p-4 filler-container">
         {children}
       </div>
     </div>
