26c6c37ca76f26501aaff3731094e40746303fbb
Karan Kiri <karankiri.96@gmail.com>
2024-05-31 17:10:06 +0530

    remove code

--- background.ts
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener((msg, sender) => {
-  // First, validate the message's structure.
-  if ((msg.from === 'content') && (msg.subject === 'showPageAction')) {
-    // Enable the page-action for the requesting tab.
-    // @ts-ignore
-    chrome.pageAction.show(sender.tab.id);
-  }
-});
